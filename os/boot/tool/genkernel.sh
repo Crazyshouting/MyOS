a15f168b445be49ef495b39549dfc647bfbdf4d1
Crazyshouting <2648311519@qq.com>
2022-12-19 14:15:52 +0800

    调试+解决kernel地址加载bug+解决putcharbug “

--- os/boot/tool/genkernel.sh
+++ os/boot/tool/genkernel.sh
@@ -1,2 +1,5 @@
 CPATH=/home/zzx/project/MyOS/MyOS/os/boot/kernel
-gcc -c $CPATH/main.c -o $CPATH/main.o && ld $CPATH/main.o -Ttext 0xc0001500 -e main -o $CPATH/kernel.bin
+gcc -m32 -I ./lib/kernel/ -c kernel/main.c -o kernel/main.o
+nasm -f elf -o lib/kernel/print.o lib/kernel/print.S
+ld -m elf_i386 -Ttext 0xc0001500 -e main -o kernel.bin kernel/main.o lib/kernel/print.o
+
